fcefd12c2c8926ac0a9b8976edffd5e5d5fc7edf
Oliver Halliday <ohallid@Olivers-MacBook-Pro-4.local>
2016-08-01 08:42:40 +0100

    new schematic

--- schematic.m
+++ schematic.m
@@ -5,7 +5,7 @@ fs2     = 17;
 
 xx      = [-2:0.01:2];
 xx1     = [1:0.01:2];
-xx2     = [0:0.01:0.5];
+xx2     = [0:0.01:0.4];
 xx3     = [0:0.01:1];
 zz1     = [0:0.01:1 ];
 zz2     = [1:0.01:2];
@@ -13,12 +13,12 @@ tt1     = [0:0.1:1];
 tt2     = [-2:0.01:0];
 tt3     = [1:0.01:2];
 pi      = 3.14159265359;
-sigma   = 0.40;
+sigma   = 0.35;
 x0      = 0;
 
 gz      = sin(pi*zz1);
 gz_out  = 0;
-fx      = exp(-((xx - x0).*(xx - x0))/(2* sigma *sigma));
+fx      = 0.8 * exp(-((xx - x0).*(xx - x0))/(2* sigma *sigma));
 xa      = 1;
 heavi1  = 0.5;
 heavi2  = 0.0;
@@ -33,12 +33,12 @@ a3 = plot(zeros(size(xx1)), xx1,'k:','LineWidth',5);
 a4  = plot(xx,ones(size(xx)),'r--', 'LineWidth',2);
 a42 = plot(xx,(ones(size(xx))+0.8),'r--', 'LineWidth',2);
 a5  = plot(zeros(size(xx3)), xx3,'b', 'LineWidth',2);
-a6  = plot(xx2,(zeros(size(xx2))+0.5),'b--', 'LineWidth',2);
-a7  = plot((zeros(size(xx2))+0.5), xx2,'b--', 'LineWidth',2);
+a6  = plot(xx2,(zeros(size(xx2))+0.4),'b--', 'LineWidth',2);
+a7  = plot((zeros(size(xx2))+0.4), xx2,'b--', 'LineWidth',2);
 M1  = 'F(x)';
 M2  = 'S(z)';
 ylim([0 2])
-set(gca, 'XTick', [0 0.5])
+set(gca, 'XTick', [0 0.4])
 set(gca, 'xticklabel', ['0';'L'],'FontSize',fs2)
 xlabel('x','FontSize', fs1);
 ylabel('z','FontSize', fs1);
